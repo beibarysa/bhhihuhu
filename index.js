0e7ab433dd19f6212436108138600e0b4c721de6
Beibarysa <beibaryszhanseit@gmail.com>
2023-06-21 21:04:30 +0600

    i did js

---
+++ index.js
@@ -0,0 +1,88 @@
+//task1 easy
+let num = 0
+while(num < 10) {
+    num = num + 1;
+    console.log(num);
+}
+
+//task2 easy
+let a = 1;
+let sum = 0;
+while (a <= 100) {
+    sum += a;
+    a++;
+    console.log(sum); 
+}
+
+let w = 1
+if((w+100)/2*100){
+    console.log(w)
+}
+
+//task3 easy
+let b = 2; 
+
+while (b < 20) {
+    b += 2; 
+    console.log(b);
+ 
+}
+
+//task4 eas y
+// function factorial(n) {
+//     let result = 1;
+//     let i = 5;
+//     while (i <= n) {
+//     result *= i;
+//     i++;
+//     }
+//     }
+//     console.log(result)
+
+//task6
+let str = "Hello";
+let i = 0; 
+while (i < str.length) {
+  console.log(str[i]); 
+  i++; 
+}
+//task7
+
+let numb = prompt("Введите число");
+for (p = 0; p <= num; p += 3) {
+  console.log(p);
+}
+
+//task 8
+const stri = "строка";
+const length = stri.length;
+
+console.log(length);
+
+//task9
+var nums = [1, 2, 3, 4, 5];
+nums.length = 5
+console.log(nums)
+
+//task10
+const arry = [20, 40, 77, 53];
+const sumy = 0;
+length() {
+    for (i = 0; i < arry.length; i++) {
+        sumy += arry[i];
+      
+    }
+}
+console.log(sumy)
+
+
+//tasl11
+let arr = [1, 8, 2, 3, 4]
+let ler = 0
+while(arr > 0)
+if(hrr > lir) {
+    console.log("положительные")
+}
+else {
+    console.log("не положительные")
+}
